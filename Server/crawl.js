de970f01af3077fa4cb4faa58e109bcbf1362f74
taemin <taemin9705@gmail.com>
2021-06-03 21:21:15 +0900

    router 업로드
    라우터 추가 업로드

--- Server/crawl.js
+++ Server/crawl.js
@@ -3,8 +3,9 @@ const firefox = require('selenium-webdriver/firefox');
 const { get } = require('selenium-webdriver/http');
 const { Type } = require('selenium-webdriver/lib/logging');
 
+const Crawl = function(){};
 
-async function Login() { 
+Crawl.Login = async function(id, pw){
   let driver = await new Builder() 
   .forBrowser('firefox')
   .setFirefoxOptions( new firefox.Options()
@@ -18,8 +19,8 @@ async function Login() {
     await (await driver.findElement(By.xpath('/html/body/div[4]/div[1]/button'))).click();
     // await driver.wait(until.elementLocated(By.xpath('//*[@id="id"]')));
     // 아이디 비밀번호 입력
-    await driver.findElement(By.id('id')).sendKeys('201663035');
-    await driver.findElement(By.id('pw')).sendKeys("Wjdtls753!");
+    await driver.findElement(By.id('id')).sendKeys(id);
+    await driver.findElement(By.id('pw')).sendKeys(pw);
 
     try{
       // 로그인 불가능시 => 서버 오류로인한
@@ -41,51 +42,50 @@ async function Login() {
   } 
 }
 
-async function getCourseList(){
-  let driver = await new Builder() 
-  .forBrowser('firefox')
-  .setFirefoxOptions( new firefox.Options()
-  .headless()
-  .windowSize({ width: 640, height: 480 })
-  .setPreference("general.useragent.override", "custom-user-agent") 
-  ).build();
-  try { 
-    // 로그인
-    await driver.get('https://cyber.anyang.ac.kr/Main.do?cmd=viewHome&userDTO.localeKey=ko');
-    await (await driver.findElement(By.xpath('/html/body/div[4]/div[1]/button'))).click();
-    // await driver.wait(until.elementLocated(By.xpath('//*[@id="id"]')));
-    // 아이디 비밀번호 입력
-    await driver.findElement(By.id('id')).sendKeys('201663035');
-    await driver.findElement(By.id('pw')).sendKeys("Wjdtls753!");
-
-    try{
-      // 로그인 불가능시 => 서버 오류로인한
-      await driver.findElement(By.linkText('로그인')).sendKeys(Key.ENTER);
+Crawl.getCourseList = async function(id, pw){
+    let driver = await new Builder() 
+    .forBrowser('firefox')
+    .setFirefoxOptions( new firefox.Options()
+    .headless()
+    .windowSize({ width: 640, height: 480 })
+    .setPreference("general.useragent.override", "custom-user-agent") 
+    ).build();
+    try { 
+      // 로그인
       await driver.get('https://cyber.anyang.ac.kr/Main.do?cmd=viewHome&userDTO.localeKey=ko');
-      await driver.navigate().refresh()
-    }
-    catch{
-      // 로그인 재시작
-      await driver.quit();
-      return '400';
-    }
-  } 
-  finally{ 
-  } 
-  // var asd = await (await driver.findElement(By.className('article'))).findElements(By.tagName('a'));
-  // for(let i=0; i<asd.length; i++) {
-  //   console.log(await asd[i].getText(), await (await asd[i].getAttribute('href')).split("W'")[1].split(',')[0]);
-  // }
+      await (await driver.findElement(By.xpath('/html/body/div[4]/div[1]/button'))).click();
+      // await driver.wait(until.elementLocated(By.xpath('//*[@id="id"]')));
+      // 아이디 비밀번호 입력
+      await driver.findElement(By.id('id')).sendKeys(id);
+      await driver.findElement(By.id('pw')).sendKeys(pw);
 
-  var courseList = await (await driver.findElement(By.tagName('select'))).findElements(By.tagName('option'));
-  for(let i=1; i<courseList.length; i++) {
-    var course = await (await courseList[i].getAttribute('value')).split(',');
-    var courseId = await course[0];
-    var courseChef = await course[1];
-    var courseName = await courseList[i].getAttribute('text')
-       console.log(courseName, courseId, courseChef);
+      try{
+        // 로그인 불가능시 => 서버 오류로인한
+        await driver.findElement(By.linkText('로그인')).sendKeys(Key.ENTER);
+        await driver.get('https://cyber.anyang.ac.kr/Main.do?cmd=viewHome&userDTO.localeKey=ko');
+        await driver.navigate().refresh()
       }
+      catch{
+        // 로그인 재시작
+        await driver.quit();
+        return '400';
+      }
+    } 
+    finally{ 
+    } 
+    // var asd = await (await driver.findElement(By.className('article'))).findElements(By.tagName('a'));
+    // for(let i=0; i<asd.length; i++) {
+    //   console.log(await asd[i].getText(), await (await asd[i].getAttribute('href')).split("W'")[1].split(',')[0]);
+    // }
 
-}
+    var courseList = await (await driver.findElement(By.tagName('select'))).findElements(By.tagName('option'));
+    for(let i=1; i<courseList.length; i++) {
+      var course = await (await courseList[i].getAttribute('value')).split(',');
+      var courseId = await course[0];
+      var courseChef = await course[1];
+      var courseName = await courseList[i].getAttribute('text')
+        console.log(courseName, courseId, courseChef);
+        }
 
-getCourseList();
+  }
+module.exports = Crawl;
--- Server/main.js
+++ Server/main.js
@@ -1,14 +1,25 @@
-const http = require('http');
-const express = require('express')
+const express = require('express');
+const session = require('express-session');
+const router = require('./router');
+
 const app = express();
-const bodyParser = require('body-parser');
+const PORT = 4000;
 
-app.use(bodyParser.urlencoded({extended: true}));
+// for read request.body
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
 
-app.set('view engine', 'ejs');
-app.set('views', './views');
-app.get('/', (req, res)=>{
-    res.render('index');
+// for use session
+app.use(session({
+    secret:'!@#ASD123asdf',
+    resave: false,
+    saveUninitialized: true
+}))
+
+// for link router
+app.use('/', router);
+
+// start server
+app.listen(PORT, ()=> {
+    console.log(`server on : http://ec2-13-209-21-68.ap-northeast-2.compute.amazonaws.com:${PORT}`);
 });
-
-app.get('/get')
